19954a890fd88fac76bfa4fa344c758cf3d4bc6e
wilona05 <wilona.muliadi@gmail.com>
2025-05-31 18:49:03 +0700

    pemrosesan query (tf-idf, normalisasi), perhitungan skor dan pemeringkatan dokumen

--- src/main/java/com/example/lucene_search/Vsm.java
+++ src/main/java/com/example/lucene_search/Vsm.java
@@ -16,13 +16,17 @@ public class Vsm {
         try{
             String indexPath = "docs/index";
             String fieldName = "tf";
-            String queryText = "gOoGle google lala";
             BytesRef term;
+            //input query
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Masukan query pencarian: ");
+            String queryText = sc.nextLine();
     
             IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
             int numDocs = reader.numDocs(); //banyak dokumen
             //System.out.println(numDocs);
 
+            //PEMROSESAN DOKUMEN-----------------------------------------------------
             //menyimpan term seluruh dokumen beserta idfnya
             Map<String, Double> vocab_idf = new HashMap<>();
             for(int i=0; i<numDocs; i++){
@@ -44,10 +48,9 @@ public class Vsm {
             // System.out.println(vocab_idf.size());
 
             //menyimpan dokumen, term yang muncul di dokumen tersebut, beserta tf-idfnya
-            Map<String, Map<String, Double>> doc_term_tfidf = new HashMap<>();
+            Map<Integer, Map<String, Double>> doc_term_tfidf = new HashMap<>();
             for(int i=0; i<numDocs; i++){
-                Map<String, Double> all_term_tfidf = new HashMap<>(); //untuk menyimpan pasangan term dan tfidf (semua term yang ada di korpus)
-                Map<String, Integer> tf_perDocs = new HashMap<>(); //untuk menyimpan pasangan term dan tf (semua term yang ada di dokumen ke-i)
+                Map<String, Double> tfidf_perDocs = new HashMap<>(); //untuk menyimpan pasangan term dan tf-idf (semua term yang ada di dokumen ke-i)
                 Terms docTerms = reader.getTermVector(i, fieldName); //ambil semua term pada dokumen ke-i
                 
                 //tf_perDocs: simpan seluruh term yang ada di dokumen ke-i, beserta tfnya
@@ -55,36 +58,103 @@ public class Vsm {
                     TermsEnum termsEnum = docTerms.iterator(); //untuk iterasi tiap term
                     while((term = termsEnum.next()) != null){
                         String termText = termsEnum.term().utf8ToString(); //ubah term menjadi string
+                        Double idfTermText = vocab_idf.get(termText); //idf dari termText
                         long tf = termsEnum.totalTermFreq(); //ambil tf dari termText
-                        tf_perDocs.put(termText, (int)tf); //simpan pasangan term dan tf
+                        //sublinear tf, weighted_tf=0 jika tf=0, selain itu weighted_tf=1+log(tf)
+                        double weighted_tf = 0;
+                        if(tf > 0){ 
+                            weighted_tf = 1 + Math.log((double)tf); 
+                        }
+
+                        double tfidf = weighted_tf * idfTermText; //hitung tf-idf
+                        tfidf_perDocs.put(termText, tfidf); //simpan pasangan term dan tf-idf
                     }
                 }
 
-                //all_term_tf_idf: simpan seluruh term yang ada di korpus, beserta tf-idfnya
-                for(Map.Entry<String, Double> entry : vocab_idf.entrySet()) { //iterasi seluruh key, value pada vocab_idf
-                    String curTerm = entry.getKey();
-                    Double curIdf = entry.getValue();
-                    Integer tf = tf_perDocs.get(curTerm); //ambil tf curTerm di dokumen ke-i, null jika curTerm tidak ada di dokumen ke-i
+                tfidf_perDocs = normalisasi(tfidf_perDocs); //normalisasi
+                doc_term_tfidf.put(i, tfidf_perDocs); //simpan seluruh pasangan term dan tfidf pada key index dokumen
+            }
+            // System.out.println(doc_term_tfidf.get(reader.document(0).get("filename")).size());
 
-                    //sublinear tf, weighted_tf=0 jika tf=0 atau tf=null, selain itu weighted_tf=1+log(tf)
-                    double weighted_tf;
-                    if(tf == null || tf <= 0){ 
-                        weighted_tf = 0;
-                    }else{
+            //PEMROSESAN QUERY-----------------------------------------------------
+            Map<String, Integer> query_term_tf = new HashMap<>(); //untuk menyimpan pasangan term dan term frequency
+            //preprocessing queryText
+            StandardAnalyzer analyzer = new StandardAnalyzer();
+            var tokenStream = analyzer.tokenStream(fieldName, new StringReader(queryText));
+            var attr = tokenStream.addAttribute(org.apache.lucene.analysis.tokenattributes.CharTermAttribute.class);
+            tokenStream.reset();
+            while(tokenStream.incrementToken()){ //iterasi tiap term pada queryText
+                String curTerm = attr.toString();
+                // simpan term dan hitung tf-df
+                // jika term belum ada, defaultValue (0) + 1
+                // jika term sudah ada, ambil valuenya kemudian tambah 1
+                query_term_tf.put(curTerm, query_term_tf.getOrDefault(curTerm, 0)+1); 
+                // System.out.println(curTerm+" "+query_term_tf.get(curTerm));
+            }
+            tokenStream.end();
+            tokenStream.close();
+            analyzer.close();
+
+            //menyimpan term yang muncul di query, beserta tf-idfnya (hanya untuk term yang muncul di korpus)
+            Map<String, Double> tfidf_query = new HashMap<>(); 
+            for(Map.Entry<String, Integer> entry : query_term_tf.entrySet()){ 
+                String curTerm =entry.getKey(); //term
+                Double idfTermText = vocab_idf.get(curTerm); //idf dari curTerm
+                if(idfTermText != null){ //term ada di korpus
+                    Integer tf = entry.getValue(); //nilai tf 
+                    //sublinear tf, weighted_tf=0 jika tf=0, selain itu weighted_tf=1+log(tf)
+                    double weighted_tf = 0;
+                    if(tf > 0){ 
                         weighted_tf = 1 + Math.log((double)tf); 
                     }
-
-                    double tfidf = weighted_tf * curIdf; //hitung tf-idf
-                    all_term_tfidf.put(curTerm, tfidf); //simpan pasangan term dan tfidf
-                    
+    
+                    double tfidf = weighted_tf * idfTermText; //hitung tf-idf
+                    tfidf_query.put(curTerm, tfidf); //simpan pasangan term dan tf-idf
+                    // System.out.println(curTerm);
+                    // System.out.println(vocab_idf.get(curTerm));
+                    // System.out.println(weighted_tf);
+                    // System.out.println(tfidf_query.get(curTerm));
                 }
+            }
+            tfidf_query = normalisasi(tfidf_query); //normalisasi
 
-                all_term_tfidf = normalisasi(all_term_tfidf); //normalisasi
-                String docName = reader.document(i).get("filename"); //nama dokumen
-                doc_term_tfidf.put(docName, all_term_tfidf); //simpan seluruh pasangan term dan tfidf pada key docName
+            //PEMERINGKATAN DOKUMEN (COSINE SIMILARITY)-----------------------------------------------------
+            Map<Integer, Double> docScores = new HashMap<>(); //untuk menyimpan skor dokumen
+            for(int i=0; i<numDocs; i++){ //iterasi seluruh dokumen
+                Map<String, Double> tfidf_curDoc = doc_term_tfidf.get(i); //ambil seluruh tf-idf dokumen ke-i
+                double score = 0.0;
+                for(Map.Entry<String, Double> entry : tfidf_query.entrySet()){ //iterasi tiap term yang ada di query
+                    String curTerm = entry.getKey(); //term
+                    Double tfidf_q = entry.getValue(); //tf-idf query
+                    Double tfidf_d = tfidf_curDoc.get(curTerm); //ambil tf-idf curTerm pada dokumen ke-i
+                    if(tfidf_d != null){ //jika curTerm ada di dokumen ke-i, tambahkan skornya
+                        score += tfidf_q*tfidf_d; 
+                    }
+                }
+                docScores.put(i, score); //simpan index dokumen dan skornya
+            }
+
+            //urutkan skor dari yang tertinggi
+            List<Map.Entry<Integer, Double>> sortedDocs = new ArrayList<>(docScores.entrySet());
+            sortedDocs.sort((a, b) -> Double.compare(b.getValue(), a.getValue())); 
+            // System.out.println(sortedDocs);
+
+            //HASIL-----------------------------------------------------
+            System.out.println("Query: " + queryText);
+            System.out.println("Dokumen:");
+            for(int i=0; i<sortedDocs.size(); i++){
+                Integer idxDoc = sortedDocs.get(i).getKey(); //index dokumen
+                Double score = sortedDocs.get(i).getValue(); //skor dokumen
+                String title = reader.document(idxDoc).get("title"); //judul dokumen
+                String fileName = reader.document(idxDoc).get("filename"); //nama dokumen
+                String category = reader.document(idxDoc).get("category"); //kategori dokumen
+                String content = reader.document(idxDoc).get("content"); //kategori dokumen
+                System.out.printf("%d skor: %.4f | judul: $s | dokumen: %s | kategori: %s\n", i+1, score, title, fileName, category);
+                System.out.println("Isi (ringkas): " +
+                        (content.length() > 100 ? content.substring(0, 100) + "..." : content));
+                System.out.println("--------------------------------------");
             }
             
-            System.out.println(doc_term_tfidf.get(reader.document(0).get("filename")).size());
         }catch(IOException e){
             e.printStackTrace();
         }
